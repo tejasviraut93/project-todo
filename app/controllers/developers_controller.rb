2d200fd592d52738205db44bf7412ebebdebfb9e
tejasviraut93 <tejasvi.raut93@gmail.com>
2019-03-17 16:52:11 +0530

    Authorize employee task

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -5,26 +5,31 @@ class DevelopersController < ApplicationController
   # GET /developers
   # GET /developers.json
   def index
+    authorize! :index, Employee
     @developers = @organization.employees.developers
   end
 
   # GET /developers/1
   # GET /developers/1.json
   def show
+    authorize! :show, Employee
   end
 
   # GET /developers/new
   def new
+    authorize! :new, Employee
     @developer = @organization.employees.new
   end
 
   # GET /developers/1/edit
   def edit
+    authorize! :edit, Employee
   end
 
   # POST /developers
   # POST /developers.json
   def create
+    authorize! :create, Employee
     @developer = @organization.employees.new(developer_params)
     @developer.password = params[:employee][:name]
     @developer.role = :developer
@@ -44,6 +49,7 @@ class DevelopersController < ApplicationController
   # PATCH/PUT /developers/1
   # PATCH/PUT /developers/1.json
   def update
+    authorize! :update, Employee
     respond_to do |format|
       if @developer.update(developer_params)
         format.html { redirect_to developer_url(@developer), notice: 'Developer was successfully updated.' }
@@ -58,6 +64,7 @@ class DevelopersController < ApplicationController
   # DELETE /developers/1
   # DELETE /developers/1.json
   def destroy
+    authorize! :destroy, Employee
     @developer.destroy
     respond_to do |format|
       format.html { redirect_to developers_url, notice: 'Developer was successfully destroyed.' }
